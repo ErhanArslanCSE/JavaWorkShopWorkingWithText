4df76b535e29eaff00345951ce79f875769397ce
erhan arslan <erhanarslan.cse@gmail.com>
2023-04-01 21:10:39 +0300

    Re initial commit

---
+++ src/main/java/org/enca/textworkshop/TextMainApp.java
@@ -0,0 +1,13 @@
+package org.enca.textworkshop;
+
+/**
+ * Hello world!
+ *
+ */
+public class TextMainApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World Erhan!" );
+    }
+}
